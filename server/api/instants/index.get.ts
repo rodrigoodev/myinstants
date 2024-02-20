6980ef4b6da13685e7a8539449b85027ebbe0e7a
rodrigorgrb@gmail.com <rodrigorgrb@gmail.com>
2024-02-20 18:24:37 -0300

    Initial commit

---
+++ server/api/instants/index.get.ts
@@ -0,0 +1,32 @@
+import * as cheerio from "cheerio";
+
+const BASE_URL = "https://www.myinstants.com";
+const READ_ALL_URL = `${BASE_URL}/pt/index/br/`;
+
+interface ReadAllResponse {
+  title: string;
+  audioUrl: string;
+}
+
+export default defineEventHandler(async (event) => {
+  const html = await $fetch<string>(READ_ALL_URL);
+  const $ = cheerio.load(html);
+
+  const response: ReadAllResponse[] = [];
+
+  $(".instant").each((index, element) => {
+    const title = $(element).children(".instant-link").text();
+    const audio = $(element).children(".small-button").attr("onclick");
+
+    if (!audio) return;
+
+    const [, audioUrl] = audio.split("'");
+
+    response.push({
+      title,
+      audioUrl: `${BASE_URL}${audioUrl}`,
+    });
+  });
+
+  return response;
+});
---
+++ server/api/instants/search.get.ts
@@ -0,0 +1,40 @@
+import * as cheerio from "cheerio";
+
+const BASE_URL = "https://www.myinstants.com";
+const SEARCH_URL = `${BASE_URL}/search/`;
+
+interface ReadAllResponse {
+  title: string;
+  audioUrl: string;
+}
+
+export default defineEventHandler(async (event) => {
+  const { q } = getQuery(event);
+
+  if (!q) {
+    throw createError({
+      status: 400,
+      message: "Query param 'q' is required",
+    });
+  }
+  const html = await $fetch<string>(`${SEARCH_URL}?name=${q}`);
+  const $ = cheerio.load(html);
+
+  const response: ReadAllResponse[] = [];
+
+  $(".instant").each((index, element) => {
+    const title = $(element).children(".instant-link").text();
+    const audio = $(element).children(".small-button").attr("onclick");
+
+    if (!audio) return;
+
+    const [, audioUrl] = audio.split("'");
+
+    response.push({
+      title,
+      audioUrl: `${BASE_URL}${audioUrl}`,
+    });
+  });
+
+  return response;
+});
